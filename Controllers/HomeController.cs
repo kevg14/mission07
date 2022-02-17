f6c825034c396e6bafdfe75c1a0e3f2e19a2e75f
kevg14 <90358194+kevg14@users.noreply.github.com>
2022-02-16 22:28:45 -0700

    mission07

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission07.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission07.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookProjectRepository repo;
+
+        public HomeController (IBookProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var inst = repo.Books.ToList();
+            return View(inst);
+        }
+
+
+    }
+}
