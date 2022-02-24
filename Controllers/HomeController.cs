7d94799ca0f9acb5ad7443c9e178ae5025389a85
kevg14 <90358194+kevg14@users.noreply.github.com>
2022-02-23 23:39:51 -0700

    mission07.2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Mission07.Models;
+using Mission07.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,9 +22,34 @@ namespace Mission07.Controllers
 
 
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index(int pageNum = 1)
         {
-            var inst = repo.Books.ToList();
+            int pageSize = 5;
+
+            var newisntance = new BooksViewModel
+            {
+               Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+               pageInfo = new PageInfo
+               {
+                   TotalNumBooks = repo.Books.Count(),
+                   BooksPerPage = pageSize,
+                   CurrentPage = pageNum
+               }
+                  
+
+
+            }
+        }
+
+            var inst = repo.Books.ToList()
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize);
+
             return View(inst);
         }
 
---
+++ Controllers/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission07.Controllers.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //I am dynamically creating the page links
+
+    }
+}
